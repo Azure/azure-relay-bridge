3d6886ac4fd0c5bf1b80a07c900a369bcd883140
Clemens Vasters <clemensv@microsoft.com>
2018-07-07 20:38:31 +0200

    grab nuget for when it's not available in build env

---
+++ src/azbridge-installer/restore.cmd
@@ -0,0 +1,5 @@
+@echo off
+cls
+if not exist ".\.nuget" mkdir ".\.nuget"
+if not exist ".\.nuget\nuget.exe" powershell -Command "Invoke-WebRequest https://www.nuget.org/nuget.exe -OutFile .\.nuget\nuget.exe"
+".nuget\NuGet.exe" restore -PackagesDirectory packages
