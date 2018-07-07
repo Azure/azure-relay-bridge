60e39bcdf3aac2923adb3fe3e884c4755d3baba0
Clemens Vasters <clemensv@microsoft.com>
2018-07-07 21:08:29 +0200

    force 32-bit build for WiX

--- src/azbridge-installer/restore.cmd
+++ src/azbridge-installer/restore.cmd
@@ -1,5 +1,4 @@
 @echo off
-cls
 if not exist ".\.nuget" mkdir ".\.nuget"
 if not exist ".\.nuget\nuget.exe" powershell -Command "Invoke-WebRequest https://www.nuget.org/nuget.exe -OutFile .\.nuget\nuget.exe"
 ".nuget\NuGet.exe" restore -PackagesDirectory packages
