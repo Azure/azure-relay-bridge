7f83cf5f04a03e956c6c4f160d693eb8cb8912a9
Clemens Vasters <clemensv@microsoft.com>
2018-07-09 20:50:24 +0200

    build props

--- package.cmd
+++ package.cmd
@@ -1,25 +1,26 @@
 @echo off
-SET _DOCKER_BUILD=true
+SET _DOCKER_BUILD="true"
 docker -v > NUL
 if not errorlevel 0 (
     echo Linux RPM and DEB packaging requires a docker install
-    SET _DOCKER_BUILD=false
+    SET _DOCKER_BUILD="false"
 )
 echo *** Sanity check Windows
 dotnet restore 
 dotnet test %*
 if not errorlevel 0 exit /b 1
 echo *** Building and packaging Windows Targets
-if "%_DOCKER_BUILD%" == "true" (
+
+if %_DOCKER_BUILD% == "true" (
+   echo *** Windows only
    msbuild /t:clean,restore,package /p:WindowsOnly=true;Configuration=Release %*
-)
-else (
+) else (
+    echo *** All platforms
     msbuild /t:clean,restore,package /p:WindowsOnly=false;Configuration=Release %*
 )
+
 if not errorlevel 0 exit /b 1
-echo *** Sanity check Unix
-docker run --rm -v %cd%:/build microsoft/dotnet:2.1-sdk dotnet restore /build/Microsoft.Azure.Relay.Bridge.sln
-docker run --rm -v %cd%:/build microsoft/dotnet:2.1-sdk dotnet test /build/Microsoft.Azure.Relay.Bridge.sln %*
-if not errorlevel 0 exit /b 1
-echo *** Building and packaging Unix/Linux Targets
-docker run --rm -v %cd%:/build microsoft/dotnet:2.1-sdk /build/package.sh %*
+if %_DOCKER_BUILD% == "true" (
+  echo *** Building and packaging Unix/Linux Targets
+  docker run --rm -v %cd%:/build microsoft/dotnet:2.1-sdk /build/package.sh /p:TargetFramework=netcoreapp2.1 %*
+)
