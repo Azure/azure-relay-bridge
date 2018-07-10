ee447b0698e6e9da0b79f24d54a00d2763e4dbaf
Clemens Vasters <clemensv@microsoft.com>
2018-07-10 14:16:12 +0200

    Revert "remove pushd/popd from outer bash scripts for VSTS"
    This reverts commit c4841446a18d730c1e3ca91affd400ef57030089.

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+pushd "${0%/*}" > /dev/null 
 dotnet msbuild /t:clean,restore,package /p:Configuration=Release $@
+popd
--- verify-build.sh
+++ verify-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+pushd "${0%/*}" > /dev/null 
 cd test/docker
 dotnet clean
 dotnet build
 dotnet test
+popd
