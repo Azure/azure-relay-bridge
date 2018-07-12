e810392e14a8efbd9f981b6eee3df1338ce3f65e
Clemens Vasters <clemensv@microsoft.com>
2018-07-12 21:37:06 +0200

    build prop take two

--- package.cmd
+++ package.cmd
@@ -1,2 +1,2 @@
-if not "%BuildNumber%"=="" then set _BuildProp="BuildNumber=%BuildNumber%"
-msbuild /t:clean,restore,build,package /p:WindowsOnly=true;Configuration=Release;%_BuildProp% %*
+if not "%BuildNumber%"=="" set _BuildProp="/p:BuildNumber=%BuildNumber%"
+msbuild /t:clean,restore,build,package /p:WindowsOnly=true;Configuration=Release %_BuildProp% %*
--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd "${0%/*}" > /dev/null 
-if [ ! -z not $BuildNumber ]; then _BuildProp="BuildNumber=$BuildNumber"; fi
-dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release;$_BuildProp $@
+if [ ! -z not $BuildNumber ]; then _BuildProp="/p:BuildNumber=$BuildNumber"; fi
+dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release $_BuildProp $@
 popd
