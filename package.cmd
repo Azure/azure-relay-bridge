09b0d155d707f7c1792b10446f9732e39ee6d030
Clemens Vasters <clemensv@microsoft.com>
2018-07-12 17:12:21 +0200

    appveyor test config

--- package.cmd
+++ package.cmd
@@ -1,9 +1,1 @@
 msbuild /t:clean,restore,build,package /p:WindowsOnly=true %*
-
-echo %cd%
-
-set xunitrunner=%userprofile%\.nuget\packages\dotnet-xunit\2.3.1\tools\net452\xunit.console.exe
-%xunitrunner% -? 2>&1 > NUL 
-if ERRORLEVEL 1 set xunitrunner=C:\Tools\xUnit20\xunit.console.exe
-
-"%xunitrunner%" "test\unit\Microsoft.Azure.Relay.Bridge.Tests\bin\Debug\net462\Microsoft.Azure.Relay.Bridge.Tests.dll" -appveyor
