42bfbbd4f3a6c4cb50fbb82c9aee9e86c6bd7c76
Clemens Vasters <clemensv@microsoft.com>
2018-07-12 21:45:41 +0200

    build number from appveyor env

--- package.cmd
+++ package.cmd
@@ -1,2 +1,2 @@
-if not "%BuildNumber%"=="" set _BuildProp="/p:BuildNumber=%BuildNumber%"
+if not "%APPVEYOR_BUILD_NUMBER%"=="" set _BuildProp="/p:BuildNumber=%APPVEYOR_BUILD_NUMBER%"
 msbuild /t:clean,restore,build,package /p:WindowsOnly=true;Configuration=Release %_BuildProp% %*
--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd "${0%/*}" > /dev/null 
-if [ ! -z not $BuildNumber ]; then _BuildProp="/p:BuildNumber=$BuildNumber"; fi
+if [ ! -z not $APPVEYOR_BUILD_NUMBER ]; then _BuildProp="/p:BuildNumber=$APPVEYOR_BUILD_NUMBER"; fi
 dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release $_BuildProp $@
 popd
