980a9cf2fade918ec0fb6a3b3419616e414c61a6
Clemens Vasters <clemensv@microsoft.com>
2018-07-12 21:26:00 +0200

    build number flow

--- package.cmd
+++ package.cmd
@@ -1,1 +1,2 @@
-msbuild /t:clean,restore,build,package /p:WindowsOnly=true %*
+if not "%BuildNumber%"=="" then set _BuildProp="BuildNumber=%BuildNumber%"
+msbuild /t:clean,restore,build,package /p:WindowsOnly=true;Configuration=Release;%_BuildProp% %*
--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pushd "${0%/*}" > /dev/null 
-dotnet msbuild /t:clean,restore,build,package $@
+if [ ! -z not $BuildNumber ]; then _BuildProp="BuildNumber=$BuildNumber"; fi
+dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release;$_BuildProp $@
 popd
