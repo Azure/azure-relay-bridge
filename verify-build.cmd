ee6c4b772bfdae4d987d5fcbb9e2a63eab859f08
Clemens Vasters <clemensv@microsoft.com>
2018-07-13 11:06:37 +0200

    build verification

--- verify-build.cmd
+++ verify-build.cmd
@@ -1,5 +1,10 @@
 pushd "%~dp0"
-%userprofile%\.nuget\packages\dotnet-xunit\2.3.1\tools\netcoreapp2.0\xunit.console.dll test\unit\Microsoft.Azure.Relay.Bridge.Tests\bin\Debug\net462\Microsoft.Azure.Relay.Bridge.Tests.dll -appveyor 
+
+set xunitrunner=%userprofile%\.nuget\packages\dotnet-xunit\2.3.1\tools\net452\xunit.console.exe
+%xunitrunner% -? 2>&1 > NUL 
+if ERRORLEVEL 1 set xunitrunner=C:\Tools\xUnit20\xunit.console.exe
+"%xunitrunner%" "test\unit\Microsoft.Azure.Relay.Bridge.Tests\bin\Debug\net462\Microsoft.Azure.Relay.Bridge.Tests.dll" -appveyor
+
 rem cd test\docker
 rem msbuild /t:clean,build,vstest
-rem popd
+popd
