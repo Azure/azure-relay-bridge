3706803c09972cb4b38f24cb4a67200fe68ca74b
Clemens Vasters <clemensv@microsoft.com>
2018-07-13 19:32:35 +0200

    build

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 pushd "${0%/*}" > /dev/null 
 if [ ! -z $APPVEYOR_BUILD_NUMBER ]; then _BuildProp="/p:BuildNumber=$APPVEYOR_BUILD_NUMBER"; fi
 if [ ! -z $APPVEYOR_BUILD_VERSION ]; then _VersionProp="/p:VersionPrefix=$APPVEYOR_BUILD_VERSION"; fi
-dotnet msbuild /t:clean,restore,build /p:Configuration=Debug $_BuildProp $_VersionProp $@
-dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release $_BuildProp $_VersionProp $@
+dotnet restore
+dotnet msbuild /t:clean,restore,build /p:Configuration=Debug /p:TargetFramework=netcoreapp2.1 $_BuildProp $_VersionProp $@
+dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release /p:TargetFramework=netcoreapp2.1 $_BuildProp $_VersionProp $@
 popd
--- verify-build.sh
+++ verify-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+if [ ! -z $APPVEYOR_BUILD_NUMBER ]; then _BuildProp="/p:BuildNumber=$APPVEYOR_BUILD_NUMBER"; fi
+if [ ! -z $APPVEYOR_BUILD_VERSION ]; then _VersionProp="/p:VersionPrefix=$APPVEYOR_BUILD_VERSION"; fi
+
 dotnet ~/.nuget/packages/dotnet-xunit/2.3.1/tools/netcoreapp2.0/xunit.console.dll test/unit/Microsoft.Azure.Relay.Bridge.Tests/bin/Debug/netcoreapp2.1/Microsoft.Azure.Relay.Bridge.Tests.dll -appveyor 
 cd test/docker
-dotnet clean
-dotnet build
-dotnet test
+dotnet clean /p:Configuration=Debug /p:TargetFramework=netcoreapp2.1 $_BuildProp $_VersionProp $@
+dotnet build /p:Configuration=Debug /p:TargetFramework=netcoreapp2.1 $_BuildProp $_VersionProp $@
+dotnet test /p:Configuration=Debug /p:TargetFramework=netcoreapp2.1 $_BuildProp $_VersionProp $@
