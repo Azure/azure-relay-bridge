9fe00188d18d00255d97ac29b623a6bad62bbc2f
Clemens Vasters <clemensv@microsoft.com>
2018-07-13 23:03:38 +0200

    test run

--- test/docker/_scripts/runtest.sh
+++ test/docker/_scripts/runtest.sh
@@ -2,13 +2,17 @@
 echo Running $_TESTNAME
 _OUTFILE=$(mktemp)
 docker run -v $_MOUNTPATH:/tests -e AZBRIDGE_TEST_CXNSTRING="$_CXNSTRING" --rm $IMAGE_NAME:latest bash /tests/$_TESTNAME.sh > $_OUTFILE
-diff $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE > /dev/null 2>&1
-_RESULT=$?
-if [ $_RESULT -eq 0 ]; then 
-   echo OK
+if [ -f $_MOUNTPATH/$_TESTNAME.reference.txt ]; then
+    diff -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE > /dev/null 2>&1
+    _RESULT=$?
+    if [ $_RESULT -eq 0 ]; then 
+        echo OK
+    else
+        diff -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE
+    fi
 else
-    cat $_OUTFILE
-    echo Error $_RESULT
-fi
+    cp $_OUTFILE $_MOUNTPATH/$_TESTNAME.reference.txt > /dev/null 2>&1
+    _RESULT=0
+fi 
 rm $_OUTFILE
 echo $_TESTNAME done.
