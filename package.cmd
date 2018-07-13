52ad5ded16636cf9473bd9dfc2a778d5fc791a21
Clemens Vasters <clemensv@microsoft.com>
2018-07-13 16:54:14 +0200

    package scripts

--- package.cmd
+++ package.cmd
@@ -1,4 +1,4 @@
 if not "%APPVEYOR_BUILD_NUMBER%"=="" set _BuildProp="/p:BuildNumber=%APPVEYOR_BUILD_NUMBER%"
 if not "%APPVEYOR_BUILD_VERSION%"=="" set _VersionProp="/p:VersionPrefix=%APPVEYOR_BUILD_VERSION%"
-msbuild /t:clean,restore,build /p:WindowsOnly=true;Configuration=Debug %_BuildProp% %_VersionProp% %*
-msbuild /t:clean,restore,build,package /p:WindowsOnly=true;Configuration=Release %_BuildProp% %_VersionProp% %*
+msbuild /t:clean,restore,build /p:WindowsOnly=true /p:Configuration=Debug %_BuildProp% %_VersionProp% %*
+msbuild /t:clean,restore,build,package /p:WindowsOnly=true /p:Configuration=Release %_BuildProp% %_VersionProp% %*
--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd "${0%/*}" > /dev/null 
-if [ ! -z not $APPVEYOR_BUILD_NUMBER ]; then _BuildProp="/p:BuildNumber=$APPVEYOR_BUILD_NUMBER"; fi
-if [ ! -z not $APPVEYOR_BUILD_VERSION ]; then _VersionProp="/p:VersionPrefix=$APPVEYOR_BUILD_VERSION"; fi
+if [ ! -z $APPVEYOR_BUILD_NUMBER ]; then _BuildProp="/p:BuildNumber=$APPVEYOR_BUILD_NUMBER"; fi
+if [ ! -z $APPVEYOR_BUILD_VERSION ]; then _VersionProp="/p:VersionPrefix=$APPVEYOR_BUILD_VERSION"; fi
 dotnet msbuild /t:clean,restore,build /p:Configuration=Debug $_BuildProp $_VersionProp $@
 dotnet msbuild /t:clean,restore,build,package /p:Configuration=Release $_BuildProp $_VersionProp $@
 popd
