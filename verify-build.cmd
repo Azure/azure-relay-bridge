5bf3295c0d3d545bde6971581a7d69412a083e33
Clemens Vasters <clemensv@microsoft.com>
2018-07-13 10:39:57 +0200

    Docker login

--- verify-build.cmd
+++ verify-build.cmd
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 %userprofile%\.nuget\packages\dotnet-xunit\2.3.1\tools\netcoreapp2.0\xunit.console.dll test\unit\Microsoft.Azure.Relay.Bridge.Tests\bin\Debug\net462\Microsoft.Azure.Relay.Bridge.Tests.dll -appveyor 
-cd test\docker
-msbuild /t:clean,build,vstest
-popd
+rem cd test\docker
+rem msbuild /t:clean,build,vstest
+rem popd
