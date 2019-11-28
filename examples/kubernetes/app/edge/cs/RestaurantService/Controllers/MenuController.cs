ce581aac7882b08257ea61680ee02c01acb9ab6a
clemensv <clemensv@microsoft.com>
2019-11-28 16:33:08 +0100

    basic app functionality

---
+++ examples/kubernetes/app/edge/cs/RestaurantService/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodPortal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<MenuController> _logger;
+
+        public MenuController(IConfiguration config, ILogger<MenuController> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            string fileName = _config.GetValue<string>("DataFile");
+            var jsonData = System.IO.File.ReadAllBytes(fileName);
+            var jsonReader = new Utf8JsonReader(jsonData, isFinalBlock: true, state: default);
+            return JsonSerializer.Deserialize<MenuItem[]>(ref jsonReader, new JsonSerializerOptions{PropertyNameCaseInsensitive=true});
+        }
+    }
+}
