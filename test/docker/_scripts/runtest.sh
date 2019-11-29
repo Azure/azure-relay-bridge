7d31fd64332734d7cb2d49f0b065c52ef9b054a1
Clemens Vasters <clemensv@microsoft.com>
2019-11-29 19:28:42 +0100

    ignore whitespace in integration test

--- test/docker/_scripts/runtest.sh
+++ test/docker/_scripts/runtest.sh
@@ -3,12 +3,12 @@ echo Running $_TESTNAME
 _OUTFILE=$(mktemp)
 docker run -v $_MOUNTPATH:/tests -e AZBRIDGE_TEST_CXNSTRING="$_CXNSTRING" --rm $IMAGE_NAME:latest bash /tests/$_TESTNAME.sh > $_OUTFILE
 if [ -f $_MOUNTPATH/$_TESTNAME.reference.txt ]; then
-    diff -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE > /dev/null 2>&1
+    diff --strip-trailing-cr -B -i -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE > /dev/null 2>&1
     _RESULT=$?
     if [ $_RESULT -eq 0 ]; then 
         echo OK
     else
-        diff -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE
+        diff --strip-trailing-cr -B -i -w $_MOUNTPATH/$_TESTNAME.reference.txt $_OUTFILE
     fi
 else
     cp $_OUTFILE $_MOUNTPATH/$_TESTNAME.reference.txt > /dev/null 2>&1
