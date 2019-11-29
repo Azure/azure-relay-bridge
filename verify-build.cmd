e61eca3db24dbded981063718daef4de5e1731c4
Clemens Vasters <clemensv@microsoft.com>
2019-11-29 20:59:22 +0100

    skip image build in PCV

--- verify-build.cmd
+++ verify-build.cmd
@@ -5,6 +5,6 @@ where %xunitrunner%  2>&1
 if ERRORLEVEL 1 set xunitrunner=C:\Tools\xUnit20\xunit.console.exe
 "%xunitrunner%" "test\unit\Microsoft.Azure.Relay.Bridge.Tests\bin\Debug\net462\Microsoft.Azure.Relay.Bridge.Tests.dll" -appveyor
 
-cd test\docker
-msbuild /t:clean,build,vstest
-popd
+rem cd test\docker
+rem msbuild /t:clean,build,vstest
+rem popd
--- verify-build.sh
+++ verify-build.sh
@@ -3,7 +3,7 @@
 if [ ! -z $APPVEYOR_BUILD_VERSION ]; then _VersionProp="/p:VersionPrefix=$APPVEYOR_BUILD_VERSION"; fi
 
 dotnet test --verbosity=normal
-cd test/docker
-dotnet clean --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
-dotnet build --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
-dotnet test --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
+#cd test/docker
+#dotnet clean --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
+#dotnet build --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
+#dotnet test --verbosity=normal /p:Configuration=Debug /p:TargetFramework=netcoreapp3.0 $_BuildProp $_VersionProp $@
