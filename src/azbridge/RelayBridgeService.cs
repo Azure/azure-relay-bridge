ec51b2a3ea62f62ac24817dc1eb38c80be5145bf
clemensv <clemensv@microsoft.com>
2019-12-16 10:11:56 +0100

    copyright header

--- src/azbridge/RelayBridgeService.cs
+++ src/azbridge/RelayBridgeService.cs
@@ -1,4 +1,7 @@
-﻿#if NET462
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if NET462
 namespace azbridge
 {
     using System;
