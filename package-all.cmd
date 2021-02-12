3a47edb831b847193946fd728c0a3b4ea28caf5f
Vlad Kolesnikov <vladkol@microsoft.com>
2021-02-11 20:14:10 -0800

    migrated to .NET 5.0

--- package-all.cmd
+++ package-all.cmd
@@ -6,21 +6,24 @@ if not errorlevel 0 (
     SET _DOCKER_BUILD="false"
 )
 echo *** Sanity check Windows
-dotnet msbuild /t:restore /p:WindowsOnly=true
-dotnet test -f net48 %*
+dotnet msbuild /t:restore /p:WindowsOnly=false /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=win10-x64
+dotnet test -f net5.0 %*
 if not errorlevel 0 exit /b 1
 echo *** Building and packaging Windows Targets
 
 if %_DOCKER_BUILD% == "true" (
    echo *** Windows only
-   dotnet msbuild /t:clean,restore,package /p:WindowsOnly=true;Configuration=Release %*
+   dotnet msbuild /t:clean,restore,package /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=win10-x64 /p:WindowsOnly=false;Configuration=Release %*
 ) else (
     echo *** All platforms
-    dotnet msbuild /t:clean,restore,package /p:WindowsOnly=false;Configuration=Release %*
+    dotnet msbuild /t:clean,restore,package /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=win10-x64 /p:WindowsOnly=false;Configuration=Release %*
+    dotnet msbuild /t:clean,restore,package /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=osx-x64 /p:WindowsOnly=false;Configuration=Release %*
+    dotnet msbuild /t:clean,restore,package /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=ubuntu.18.04-x64 /p:WindowsOnly=false;Configuration=Release %*
+    dotnet msbuild /t:clean,restore,package /p:TargetFramework=netcoreapp5.0 /p:RuntimeIdentifier=debian.10-x64 /p:WindowsOnly=false;Configuration=Release %*
 )
 
 if not errorlevel 0 exit /b 1
 if %_DOCKER_BUILD% == "true" (
   echo *** Building and packaging Unix/Linux Targets
-  docker run --rm -v %cd%:/build mcr.microsoft.com/dotnet/core/sdk:3.1-buster /build/package.sh /p:TargetFramework=netcoreapp3.1 %*
+  docker run --rm -v %cd%:/build mcr.microsoft.com/dotnet/sdk:5.0 /build/package.sh %*
 )
--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
 if not "%APPVEYOR_BUILD_VERSION%"=="" set _VersionProp="/p:VersionPrefix=%APPVEYOR_BUILD_VERSION%"
 if not "%BUILDVERSION%"=="" set _VersionProp="/p:VersionPrefix=%BUILDVERSION%"
-msbuild /t:clean,build,package /p:WindowsOnly=true /p:Configuration=Release %_BuildProp% %_VersionProp% %*
+msbuild /t:clean,build,package /p:WindowsOnly=false /p:RuntimeIdentifier=win10-x64 /p:RuntimeIdentifier=win10-x64 /p:Configuration=Release %_BuildProp% %_VersionProp% %*
