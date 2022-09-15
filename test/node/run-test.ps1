fee0e49582abd815da4a0d7aa5e9ac3c8a8b3062
Clemens Vasters <clemensv@microsoft.com>
2022-09-15 09:47:59 -0700

    Node Test
    Signed-off-by: Clemens Vasters <clemensv@microsoft.com>

--- test/node/run-test.ps1
+++ test/node/run-test.ps1
@@ -2,13 +2,13 @@ Param (
 [string]$AzBridgeLocation
 )
 
-$azbridge1 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-T","a1:3000","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru >> remlog.log
-$nodeserver = Start-Process -FilePath "node" -ArgumentList "index.js" -PassThru >> srvlog.log
-$azbridge2 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-L","3001:a1","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru >> loclog.log
+$azbridge1 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-T","a1:http/3000","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru 
+$nodeserver = Start-Process -FilePath "node" -ArgumentList "index.js" -PassThru
+$azbridge2 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-L","3001/http:a1","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru 
 Start-Sleep -Seconds 5
 
 wget -Uri "http://localhost:3001" > downloaded.txt
 
-Stop-Process $nodeserver
-Stop-Process $azbridge1
-Stop-Process $azbridge2
+#Stop-Process -Id $nodeserver.Id
+#Stop-Process -Id $azbridge1.Id
+#Stop-Process -Id $azbridge2.Id
