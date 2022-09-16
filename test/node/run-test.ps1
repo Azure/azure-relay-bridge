8889f46ca7aea71be2a3fc39197264e1e65fc369
Clemens Vasters <clemens@vasters.com>
2022-09-15 23:59:24 -0700

    Update run-test.ps1

--- test/node/run-test.ps1
+++ test/node/run-test.ps1
@@ -2,9 +2,9 @@ Param (
 [string]$AzBridgeLocation
 )
 
-$azbridge1 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-T","a1:3000","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru 
+$azbridge1 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-T","a1:3000","-x","$Env:AZBRIDGE_TEST_CXNSTRING","-v" -PassThru 
 $nodeserver = Start-Process -FilePath "node" -ArgumentList "index.js" -PassThru
-$azbridge2 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-L","3001:a1","-x",$Env:AZBRIDGE_TEST_CXNSTRING,"-v" -PassThru 
+$azbridge2 = Start-Process -FilePath $AzBridgeLocation\azbridge.exe -ArgumentList "-L","3001:a1","-x","$Env:AZBRIDGE_TEST_CXNSTRING","-v" -PassThru 
 Start-Sleep -Seconds 15
 
 
