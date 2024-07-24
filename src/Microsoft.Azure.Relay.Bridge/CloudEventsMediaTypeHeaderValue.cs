573a22f015ab9498dbadd50995a53cac42d1eb69
Roberto Cervantes <robece@protonmail.com>
2024-07-23 18:35:44 -0700

    Added Cloud Events media type header support
    This commit removes the error when forwarding requests with application/cloudevents+json; charset=utf-8 content media format, and the vulnerability issue with the Azure.Identity SDK.

---
+++ src/Microsoft.Azure.Relay.Bridge/CloudEventsMediaTypeHeaderValue.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+
+namespace Microsoft.Azure.Relay.Bridge
+{
+    internal class CloudEventsMediaTypeHeaderValue : MediaTypeHeaderValue
+    {
+        public CloudEventsMediaTypeHeaderValue(string mediaType) : base(mediaType)
+        {
+        }
+
+        public CloudEventsMediaTypeHeaderValue(string mediaType, string charset) : base(mediaType)
+        {
+            Parameters.Add(new NameValueHeaderValue("charset", charset));
+        }
+
+        public static bool TryParse(string input, out CloudEventsMediaTypeHeaderValue parsedValue)
+        {
+            parsedValue = null;
+
+            if (!MediaTypeHeaderValue.TryParse(input, out var mediaType))
+            {
+                return false;
+            }
+
+            if (mediaType.MediaType != "application/cloudevents+json")
+            {
+                return false;
+            }
+
+            var charset = mediaType.CharSet ?? "utf-8";
+            parsedValue = new CloudEventsMediaTypeHeaderValue("application/cloudevents+json", charset);
+
+            return true;
+        }
+    }
+}
